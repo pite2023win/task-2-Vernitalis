10c6631d88c3721364655c0b957488a45f7491bf
Vernitalis <147503735+Vernitalis@users.noreply.github.com>
2023-10-24 13:37:15 +0200

    Update task.py

--- task.py
+++ task.py
@@ -1,26 +1,47 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
+# Banking simulator. Write a code in python that simulates the banking system.
+# The program should:
 # - be able to create new banks
 # - store client information in banks
 # - allow for cash input and withdrawal
 # - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python task.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+class Client:
+    def __init__(self, client_name, client_surname, client_age, client_balance):
+        self.client_name = client_name
+        self.client_surname = client_surname
+        self.client_age = client_age
+        self.client_balance = client_balance
+
+    client_balance = 1000
+
+
+class Bank:
+    def __init__(self, name):
+        self.name = name
+
+    client_list = []
+
+    def add_client(self):
+        pass
+
+    def deposit(self, client_id, deposit_ammount):
+        client_id.balance += deposit_ammount
+
+    def withdraw(self, client_id, withdraw_ammount):
+        if Client.client_balance < withdraw_ammount:
+            print("not enough money to withdraw")
+        else:
+            client_id.balance -= withdraw_ammount
+
+    def transfer_balance(self, client1_id, client2_id, transfer_ammount):
+        if client1_id.client_balance < transfer_ammount:
+            print("not enough money to transfer")
+        else:
+            client1_id.client_balance -= transfer_ammount
+            client2_id.client_balance += transfer_ammount
+
+
+if __name__ == "__main__":
+    Bank("test_bank")
+    Client("Test", "Client", 45, 2500)
+
+
